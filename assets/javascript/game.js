cb803de5a46b612bea3bc85e81c272b842d40e4c
Nick Kidder <nickidder@yahoo.com>
2019-06-08 14:04:17 -0700

    updated javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
-const word = ["acdc", "motleycrue", "kiss", "twistedsister"];
+var word = ['bread', 'corn', 'rice','meat']
+//["acdc", "motleycrue", "kiss", "twistedsister"];
 var randNum = Math.floor(Math.random() * word.length);
 var chosenWord = word[randNum];
 var rightWord = [];
 var wrongWord = [];
 var underScore = [];
-console.log(chosenWord[0]);
+var chosenWordArr = chosenWord.split("");
+console.log(chosenWordArr);
+console.log(chosenWord);
 var underScoreDoc = document.getElementsByClassName("underScore");
 var generateUnderscore = () => {
     for (var i = 0; i < chosenWord.length; i++) {
@@ -13,22 +16,42 @@ var generateUnderscore = () => {
     return underScore;
 };
 console.log(generateUnderscore());
-document.addEventListener('keypress', (event) => {
-    var keycode = event.keyCode;
-    console.log(keycode);
-    var keyword = String.fromCharCode(keycode);
-    console.log(keyword)
-    if (chosenword.indexOf(keyord) > -1) {
+// document.addEventListener('keypress', (event) => {
+//     var keycode = event.keyCode;
+//     console.log(keycode);
+//     var keyword = String.fromCharCode(keycode);
+//     console.log(keyword)
+//     if (chosenWord.indexOf(keyword) > -1) {
+//         console.log(true);
+
+//         rightWord.push(keyword);
+//         console.log(rightWord);
+//         underScore[chosenWord.indexOf(keyword)] = keyword;
+//         console.log(underScore);
+//         if (underScore.join("") == chosenWord) {
+//             alert("You Won!")
+//         }  
+//         else {
+//             wrongWord.push(wrongWord);
+//             console.log(wrongWord);
+//         }
+//     }
+// });
+document.onkeyup = function (e) {
+    var keyword = event.key
+    if (chosenWord.indexOf(keyword) > -1) {
         console.log(true);
 
         rightWord.push(keyword);
         console.log(rightWord);
         underScore[chosenWord.indexOf(keyword)] = keyword;
         console.log(underScore);
-        if (underScore.join("") == chosenWord);
-        alert("You Won!")
-    }    else {
-        wrongWord.push(wrongWord);
-        console.log(wrongWord);
+        if (underScore.join("") == chosenWord) {
+            alert("You Won!")
+        }  
+        else {
+            wrongWord.push(wrongWord);
+            console.log(wrongWord);
+        }
     }
-});
+}
